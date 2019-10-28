1ed3ec05bb98aec36218f5c5fe70c81d8ef853fb
jtdev-hee <55820250+jtdev-hee@users.noreply.github.com>
2019-10-28 17:08:14 +0900

    Create main.js

---
+++ jquery-tooptip/js/main.js
@@ -0,0 +1,28 @@
+/*
+ * File       : js/main.js
+ * Author     : STUDIO-JT (HEE)
+ * Guideline  : JTstyle.1.0
+ *
+ * SUMMARY:
+ * 1) 
+ * 2)
+ * 3) 
+ */
+
+
+jQuery(function($) {
+
+hee_tooltip();
+
+function hee_tooltip(){
+
+    $('.my_tooltip').hee_tooltip({
+		backgroundColor: '#e9e9e9',
+		color: '#0b21ee'
+	});
+    
+}
+
+
+
+})
